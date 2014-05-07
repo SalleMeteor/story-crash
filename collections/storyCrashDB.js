77946c17279f351c5729e10a658989ff94f36201
SalleMeteor <SalleMeteor@users.noreply.github.com>
2014-05-07 14:27:23 +0200

    Update storyCrashDB.js

--- collections/storyCrashDB.js
+++ collections/storyCrashDB.js
@@ -1,31 +1,13 @@
-if (Meteor.isClient) {
-  Meteor.startup(function () {
-     
-        storyCrashDB = new Meteor.Collection("contacts");
-        
-        storyCrashDB.allow({
-          insert: function() {
-            return true;
-          }
-        });
-   });
-}
+storyCrashDB = new Meteor.Collection("contacts");
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-     
-        storyCrashDB = new Meteor.Collection("contacts");
-        
-        storyCrashDB.allow({
-          insert: function() {
-            return true;
-          },
-          update: function() {
-            return true;
-          },	
-          remove: function() {
-            return true;
-          }
-        });
-   });
-}
+storyCrashDB.allow({
+  insert: function() {
+    return true;
+  },
+  update: function() {
+    return true;
+  },	
+  remove: function() {
+    return true;
+  }
+});
